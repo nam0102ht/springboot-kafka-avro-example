a7d8f3da3d8737153ad3d7f709f8142feb483eff
Radan <namnha@gosoft.co.th>
2022-12-18 19:57:52 +0700

    add code pid

--- src/main/java/com/ntnn/Application.java
+++ src/main/java/com/ntnn/Application.java
@@ -2,10 +2,17 @@ package com.ntnn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter(projectPath  + "/tmp/app.pid"));
+        springApplication.run(args);
     }
 }
