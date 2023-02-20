de4bdae0ba1e10bf6d13cb9e5d8b80a42b7d7387
Radan Nguyen <nguyentrungnhatnam@gmail.com>
2023-02-20 22:33:32 +0700

    add harness env config

--- src/main/java/com/ntnn/Application.java
+++ src/main/java/com/ntnn/Application.java
@@ -1,12 +1,13 @@
 package com.ntnn;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
-import java.io.File;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         String projectPath = System.getProperty("user.dir");
