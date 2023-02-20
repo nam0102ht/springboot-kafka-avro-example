4fceba6dc2eb95708bb0355da9d46ebe0c2a90f4
Radan Nguyen <nguyentrungnhatnam@gmail.com>
2023-02-20 22:37:39 +0700

    add unit test

---
+++ src/test/java/unit/com/ntnn/RequestListenerTest.java
@@ -0,0 +1,11 @@
+package unit.com.ntnn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RequestListenerTest {
+    @Test
+    public void test() {
+        Assert.assertEquals("1", "1");
+    }
+}
